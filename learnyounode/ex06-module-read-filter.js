c2e884ed828e2aa8791edf86707bdf509afd962d
Rachelle Gray <msrachellegray@gmail.com>
2016-05-03 13:35:04 -0700

    learn you node ex 6 make a module - convert to spaces

--- learnyounode/ex06-module-read-filter.js
+++ learnyounode/ex06-module-read-filter.js
@@ -3,30 +3,30 @@
 
 module.exports = function readFilter(args) {
 
-	var fs;
-	var pathToProcess;
-	var fileExtension;
-	var regularExpressionPattern;
+    var fs;
+    var pathToProcess;
+    var fileExtension;
+    var regularExpressionPattern;
 
-	fs = require('fs');
-	pathToProcess = process.argv[2];
-	fileExtension = process.argv[3];
-	regularExpressionPattern = new RegExp('^[^.]*\.' + fileExtension);
+    fs = require('fs');
+    pathToProcess = process.argv[2];
+    fileExtension = process.argv[3];
+    regularExpressionPattern = new RegExp('^[^.]*\.' + fileExtension);
 
-	fs.readdir(pathToProcess, function (err, list) {
-		'use strict';
-		if (err) {
-			console.log(err);
-			return err;
-		}
-		list.filter(function evaluateMatch(item) {
-			return regularExpressionPattern.test(item);
-		})
-			.map(function logEachPositive(item) {
+    fs.readdir(pathToProcess, function (err, list) {
+        'use strict';
+        if (err) {
+            console.log(err);
+            return err;
+        }
+        list.filter(function evaluateMatch(item) {
+            return regularExpressionPattern.test(item);
+        })
+            .map(function logEachPositive(item) {
                 console.log(item);
             });
-	});
+    });
 
-	// some brilliant code here
-	console.log('My brilliant module with ' + args);
+    // some brilliant code here
+    console.log('My brilliant module with ' + args);
 }
