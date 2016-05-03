afdab4154976b07d976d63598308368c22508919
Rachelle Gray <msrachellegray@gmail.com>
2016-05-03 13:32:00 -0700

    learn you node ex 6 make a module - wip

---
+++ learnyounode/ex06-modular.js
@@ -0,0 +1,6 @@
+// use a module to read and filter
+
+var readFilter = require('./readFilter');// the 'js' extension is optional here and you'll see it omitted frequently
+
+console.log(readFilter);
+// readFilter('monkeys');
---
+++ learnyounode/ex06-module-read-filter.js
@@ -0,0 +1,32 @@
+// directory reading and filtering module
+
+
+module.exports = function readFilter(args) {
+
+	var fs;
+	var pathToProcess;
+	var fileExtension;
+	var regularExpressionPattern;
+
+	fs = require('fs');
+	pathToProcess = process.argv[2];
+	fileExtension = process.argv[3];
+	regularExpressionPattern = new RegExp('^[^.]*\.' + fileExtension);
+
+	fs.readdir(pathToProcess, function (err, list) {
+		'use strict';
+		if (err) {
+			console.log(err);
+			return err;
+		}
+		list.filter(function evaluateMatch(item) {
+			return regularExpressionPattern.test(item);
+		})
+			.map(function logEachPositive(item) {
+                console.log(item);
+            });
+	});
+
+	// some brilliant code here
+	console.log('My brilliant module with ' + args);
+}
