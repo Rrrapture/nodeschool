e7cb954c804f5448c7879816243959d31a4cd544
Rachelle Gray <msrachellegray@gmail.com>
2016-05-10 15:32:07 -0700

    Learn you node - exercise 7 - http client:  - initial setup of HTTP get request

---
+++ learnyounode/ex07-http-client.js
@@ -0,0 +1,29 @@
+// exercise 07 HTTP client
+//
+// mission: perform HTTP GET request
+// URL will be first command-line argument (argv)
+// write String contents of each 'data' event from the request to the console,
+// one per line (stdout)
+//
+// use http core module
+// https://nodejs.org/docs/latest-v5.x/api/http.html
+// Node's HTTP API is low level; it deals with stream handling and message parsing only
+// https://nodejs.org/docs/latest-v5.x/api/http.html#http_http_get_options_callback
+// pass a command line argument like this:
+// $node ex07-http-client.js http://rrrapture.com
+
+var http,
+    url;
+
+// http.get(options[, callback])
+
+http = require('http');
+url = process.argv[2];
+
+http
+    .get(url, function getRequest(res) {
+        console.log('Got response: ' + res.statusCode);
+    })
+    .on('error', function detailError(error) {
+        console.log('Got error: ' + error);
+    });
